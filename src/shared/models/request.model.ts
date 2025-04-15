cc1048896e05da6a77f034d6de7af840d0e89cdf
Le Hoang Trong <langtupro0456@gmail.com>
2025-04-15 07:17:54 +0700

    Xử lý model và dto cho Brand và BrandTranslation

--- src/shared/models/request.model.ts
+++ src/shared/models/request.model.ts
@@ -1,6 +1,12 @@
 import { z } from 'zod'
 
+export const PaginationQuerySchema = z.object({
+  page: z.coerce.number().int().positive().default(1), // Phải thêm coerce để chuyển từ string sang number
+  limit: z.coerce.number().int().positive().default(10), // Phải thêm coerce để chuyển từ string sang number
+})
+
 // Nếu sau này có method chuyền lên empty body rỗng thì có thể sử dụng lại cái schema này
 export const EmptyBodySchema = z.object({}).strict()
 
 export type EmptyBodyType = z.infer<typeof EmptyBodySchema>
+export type PaginationQueryType = z.infer<typeof PaginationQuerySchema>
