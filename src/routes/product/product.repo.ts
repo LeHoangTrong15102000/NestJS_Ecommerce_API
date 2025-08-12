13a8aaacbfc0387e6bc3221857f0d4da061bffeb
Le Hoang Trong <langtupro0456@gmail.com>
2025-08-12 17:06:40 +0700

    thực hiện sortBy và orderBy cho product.

--- src/routes/product/product.repo.ts
+++ src/routes/product/product.repo.ts
@@ -8,7 +8,7 @@ import {
   ProductType,
   UpdateProductBodyType,
 } from 'src/routes/product/product.model'
-import { ALL_LANGUAGE_CODE } from 'src/shared/constants/other.constant'
+import { ALL_LANGUAGE_CODE, OrderByType, SortBy, SortByType } from 'src/shared/constants/other.constant'
 import { PrismaService } from 'src/shared/services/prisma.service'
 
 @Injectable()
@@ -26,6 +26,8 @@ export class ProductRepo {
     createdById,
     isPublic,
     languageId,
+    orderBy,
+    sortBy,
   }: {
     limit: number
     page: number
@@ -37,6 +39,8 @@ export class ProductRepo {
     createdById?: number
     isPublic?: boolean
     languageId: string
+    orderBy: OrderByType
+    sortBy: SortByType
   }): Promise<GetProductsResType> {
     const skip = (page - 1) * limit
     const take = limit
@@ -82,6 +86,21 @@ export class ProductRepo {
         lte: maxPrice,
       }
     }
+    // Mặc định sort theo createdAt mới nhất
+    let caculatedOrderBy: Prisma.ProductOrderByWithRelationInput | Prisma.ProductOrderByWithRelationInput[] = {
+      createdAt: orderBy,
+    }
+    if (sortBy === SortBy.Price) {
+      caculatedOrderBy = {
+        basePrice: orderBy,
+      }
+    } else if (sortBy === SortBy.Sale) {
+      caculatedOrderBy = {
+        orders: {
+          _count: orderBy,
+        },
+      }
+    }
     const [totalItems, data] = await Promise.all([
       this.prismaService.product.count({
         where,
@@ -92,10 +111,14 @@ export class ProductRepo {
           productTranslations: {
             where: languageId === ALL_LANGUAGE_CODE ? { deletedAt: null } : { languageId, deletedAt: null },
           },
+          orders: {
+            where: {
+              deletedAt: null,
+              status: 'DELIVERED',
+            },
+          },
         },
-        orderBy: {
-          createdAt: 'desc',
-        },
+        orderBy: caculatedOrderBy,
         skip,
         take,
       }),
