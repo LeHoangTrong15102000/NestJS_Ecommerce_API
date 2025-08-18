a7412b14b39e1906facc717f7f46f750e8938831
Le Hoang Trong <langtupro0456@gmail.com>
2025-08-18 20:44:54 +0700

    Custom websokcet adapter.

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { NestExpressApplication } from '@nestjs/platform-express'
 import { UPLOAD_DIR } from 'src/shared/constants/other.constant'
+import { WebsocketAdapter } from 'src/websockets/websocket.adapter'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
   app.enableCors() // Enable CORS for all routes
+  app.useWebSocketAdapter(new WebsocketAdapter(app))
   // app.useStaticAssets(UPLOAD_DIR, {
   //   prefix: '/media/static',
   // })
