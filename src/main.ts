8d6479359ee8d242ef3ccfa401fe05f1b19e1314
Le Hoang Trong <langtupro0456@gmail.com>
2025-08-22 15:13:06 +0700

    Thêm swagger vào bên trong cho cái hệ thống.

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,32 @@ import { AppModule } from './app.module'
 import { NestExpressApplication } from '@nestjs/platform-express'
 import { UPLOAD_DIR } from 'src/shared/constants/other.constant'
 import { WebsocketAdapter } from 'src/websockets/websocket.adapter'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { patchNestJsSwagger } from 'nestjs-zod'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
   app.enableCors() // Enable CORS for all routes
+  patchNestJsSwagger()
+  const config = new DocumentBuilder()
+    .setTitle('Ecommerce API')
+    .setDescription('The API for the ecommerce application')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .addApiKey(
+      {
+        name: 'authorization',
+        type: 'apiKey',
+      },
+      'payment-api-key',
+    )
+    .build()
+  const documentFactory = () => SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, documentFactory, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  })
   // app.useWebSocketAdapter(new WebsocketAdapter(app))
   const websocketAdapter = new WebsocketAdapter(app)
   await websocketAdapter.connectToRedis()
--- src/routes/payment/payment.controller.ts
+++ src/routes/payment/payment.controller.ts
@@ -5,8 +5,10 @@ import { ZodSerializerDto } from 'nestjs-zod'
 import { Auth } from 'src/shared/decorators/auth.decorator'
 import { WebhookPaymentBodyDTO } from 'src/routes/payment/payment.dto.'
 import { AuthType } from 'src/shared/constants/auth.constant'
+import { ApiSecurity } from '@nestjs/swagger'
 
 @Controller('payment')
+@ApiSecurity('payment-api-key')
 export class PaymentController {
   constructor(private readonly paymentService: PaymentService) {}
 
